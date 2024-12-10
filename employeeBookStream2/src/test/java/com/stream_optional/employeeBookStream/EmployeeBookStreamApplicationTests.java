28b42e2a231a4558a3aeaeb5bc4d8ee0bfaa54e1
Евгений <Safronovbr@mail.ru>
2024-12-10 14:10:04 +0300

    Started

---
+++ employeeBookStream2/src/test/java/com/stream_optional/employeeBookStream/EmployeeBookStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stream_optional.employeeBookStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
